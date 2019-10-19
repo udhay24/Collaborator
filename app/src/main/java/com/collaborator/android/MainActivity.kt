b730fafa6d30333fdc5fff3ba9990adf04b568eb
udhay24 <udhay@rivi.co>
2019-10-19 18:44:39 +0530

    added firestore

--- app/src/main/java/com/collaborator/android/MainActivity.kt
+++ app/src/main/java/com/collaborator/android/MainActivity.kt
@@ -2,11 +2,29 @@ package com.collaborator.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    // Access a Cloud Firestore instance from your Activity
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = db.collection("issues")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Toast.makeText(this.applicationContext, document.data.toString(), Toast.LENGTH_SHORT).show()
+                    Log.d("Main", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Main", "Error getting documents.", exception)
+            }
     }
 }
